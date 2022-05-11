df5bbc6c6a3ae274f19818c3e1ce798c42cb38a3
OctoHuman <17958767+OctoHuman@users.noreply.github.com>
2022-05-11 10:34:27 -0500

    Remove unneeded files

--- file_tracker/file_metadata_tracker.py
+++
@@ -1,6 +0,0 @@
-"""
-Track metadata by logging file hash, size, and mod time (possibly also extended attributes).
-Allow scanning of files to update this database when file info changes.
-Furthermore, then be able to scan this database to determine likely file duplicates.
-If a potential duplicate is found, verifiy that file matches by doing a byte-by-byte comparison, finally replacing the duplicate with a hardlink.
-"""
